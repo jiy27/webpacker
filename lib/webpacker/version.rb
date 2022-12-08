2eb58603125fed7d74010f906f9ed642a780d186
jiy27 <sinsea@hotmail.com>
2022-12-08 16:38:25 +0800

    Update version.rb

--- lib/webpacker/version.rb
+++ lib/webpacker/version.rb
@@ -1,8 +1,4 @@
 module Webpacker
   # Change the version in package.json too, please!
-<<<<<<< HEAD
-  VERSION = "6.0.0.rc.4".freeze
-=======
   VERSION = "6.0.0.beta.7".freeze
->>>>>>> parent of e1f56d0 (Merge branch 'master' into projectuse)
 end
